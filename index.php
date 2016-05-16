c5780037ee7ece01ae11f71b6a1c548073ae957e
Alex Lich <psixwill@gmail.com>
2016-05-16 22:36:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>AlexLiCH</title>
+</head>
+<body>
+<suture>
+   <heder>
+    <p >AlexLiCH</p>
+    </heder>
+    <content>
+       <nav>
+          <a href="1.html">Главная страница</a>
+          <a href="2.html">Резюме</a>
+          <a href="3.html">Портфолио</a>
+          <a href="4.html">Фотоальбом</a>
+          <a href="4.html">Игры</a>
+          <a href="4.html">Контакты</a> 
+       </nav>
+        
+    </content>
+</suture>
+</body>
+</html>
