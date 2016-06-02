c73fe89467fdbfd63560b301fcff31ae28723e1f
iGilga <pyua87@gmail.com>
2016-06-03 01:27:53 +0300

    Fix tabs code style and set date timezone

--- src/app/views/templateView.php
+++ src/app/views/templateView.php
@@ -1,41 +1,43 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" type="text/css" href="src/assets/css/styles.css">
-</head>
-<body>
-	<div class="container">
-		<div class="notepad">
-			<div class="suture">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="src/assets/css/styles.css" >
+    </head>
 
-				<div class="head">
-					<p>AlexLiCH</p>
-				</div>
+    <body>
+    	<div class="container">
+    		<div class="notepad">
+    			<div class="suture">
 
-				<div class="toilet_paper">
-					<nav>
-					  <a href="/">Главная страница</a>
-					  <a href="/resume">Резюме</a>
-					  <a href="/portfolio">Портфолио</a>
-					  <a href="/photoalbum">Фотоальбом</a>
-					  <a href='/about'>Контакты</a>
-					</nav>
+    				<div class="head">
+    					<p>AlexLiCH</p>
+    				</div>
 
-                    <div>
-                        <?php include $contentView; ?>
-                    </div>
+    				<div class="toilet_paper">
+    					<nav>
+    					  <a href="/">Главная страница</a>
+    					  <a href="/resume">Резюме</a>
+    					  <a href="/portfolio">Портфолио</a>
+    					  <a href="/photoalbum">Фотоальбом</a>
+    					  <a href='/about'>Контакты</a>
+    					</nav>
 
-				    <div id="content5">
-						<ul class="footer" style="text-align=center">
-                            <li><a href="https://vk.com/alex__lich">2016 - <?echo date("Y");?> ©Develop by AlexLiCH</a></li>
-						    <li><a href="/">О сайте</a></li>
-						    <li><a href='/about'>Обратная связь</a></li>
-						</ul>
-					</div>
+                        <div>
+                            <?php include $contentView; ?>
+                        </div>
 
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
+    				    <div id="content5">
+    						<ul class="footer" style="text-align=center">
+                                <li><a href="https://vk.com/alex__lich">2016 - <?php date_default_timezone_set('Europe/Moscow'); echo date("Y");?> ©Develop by AlexLiCH</a></li>
+    						    <li><a href="/">О сайте</a></li>
+    						    <li><a href='/about'>Обратная связь</a></li>
+    						</ul>
+    					</div>
+
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </body>
 </html>
