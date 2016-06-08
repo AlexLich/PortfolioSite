85b5872b1a766f3321a954792c9f176bb68908ba
iGilga <pyua87@gmail.com>
2016-06-08 20:02:29 +0300

    Добавлен сервис авторизация. Функция как заглушилки для теста.

---
+++ src/app/services/authService.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Service;
+
+class AuthService
+{
+    public function login($user, $password)
+    {
+        return true;
+    }
+
+    public function logout()
+    {
+
+    }
+}
