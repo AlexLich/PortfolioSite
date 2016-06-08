20f4b82ce0ee531b0eb14baf7c749e7f089bebf8
Alex Lich <psixwill@gmail.com>
2016-06-08 16:54:04 +0300

    добавил MVC логина

---
+++ src/app/views/loginView.php
@@ -0,0 +1,9 @@
+	<div class="content2">
+	<form action="/login" method="post">
+			<label for="txtUser">Логин</label>
+			<input id="txtUser" type="text" name="login" value="" /></br>
+			<label for="txtString">Пароль</label>
+        <input id="txtString" type="password" name="pw" /></br>
+			<button type="submit">Войти</button>
+	</form>
+</div>
