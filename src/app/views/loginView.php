00bcabe17caf6b0fef872ccfc2673e5ef1a89cae
Alex Lich <psixwill@gmail.com>
2016-06-08 17:46:29 +0300

    добавил дивки для выравнения логина

--- src/app/views/loginView.php
+++ src/app/views/loginView.php
@@ -1,9 +1,16 @@
+	
 	<div class="content2">
 	<form action="/login" method="post">
+        <div class="log">
 			<label for="txtUser">Логин</label>
-			<input id="txtUser" type="text" name="login" value="" /></br>
+			<input id="txtUser" type="text" name="login" value="" />
+        </div>
+        <div class="log1">
 			<label for="txtString">Пароль</label>
-        <input id="txtString" type="password" name="pw" /></br>
+        <input id="txtString" type="password" name="pw" />
+        </div>
+        <div class="btt">
 			<button type="submit">Войти</button>
+        </div>
 	</form>
 </div>
