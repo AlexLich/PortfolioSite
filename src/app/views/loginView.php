29acd149af15d6a6db29f2de591b59a54866f800
iGilga <pyua87@gmail.com>
2016-06-09 03:17:17 +0300

    Переименованы имя переменных

--- src/app/views/loginView.php
+++ src/app/views/loginView.php
@@ -1,13 +1,13 @@
-	
+
 	<div class="content2">
 	<form action="/login" method="post">
         <div class="log">
 			<label for="txtUser">Логин</label>
-			<input id="txtUser" type="text" name="login" value="" />
+			<input id="txtUser" type="text" name="username" value="" />
         </div>
         <div class="log1">
 			<label for="txtString">Пароль</label>
-        <input id="txtString" type="password" name="pw" />
+        <input id="txtString" type="password" name="password" />
         </div>
         <div class="btt">
 			<button type="submit">Войти</button>
