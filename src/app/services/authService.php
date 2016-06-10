dc72d74b5c145ab0f680bef8c984e1c3457f6eef
Alex Lich <psixwill@gmail.com>
2016-06-11 00:00:11 +0300

    переделана логика логоут логина, добавлена функция авторизации

--- src/app/services/authService.php
+++ src/app/services/authService.php
@@ -14,12 +14,14 @@ class AuthService
     public function login($username, $password)
     {
         $result = false;
+        
         if ($username and $password) {
             $user = $this->findUser($username);
             if ($user) {
-                // session_start();
                 if (strpos($user['password'], $password) !== false) {
-                    // $_SESSION['admin'] = true;
+                    session_start();
+                    $_SESSION['admin'] = true;
+                    session_write_close();
                     $result = true;
                 }
             }
@@ -30,30 +32,41 @@ class AuthService
 
     public function logout()
     {
+        session_start();
         session_destroy();
-        header('Location: /login');
     }
 
     public function findUser($username)
     {
         $result = false;
 
-        // if(!is_file(FILE_NAME)) {
-        //     $users = file(FILE_NAME);
-        //
-        //     foreach($users as $user) {
-        //         if(strpos($user, $username.':') !== false) {
-        //             $result = $user;
-        //         }
-        //     }
-        // }
+//         if(is_file(FILE_NAME)) {
+//             $users = file(FILE_NAME);
+//        
+//             foreach($users as $user) {
+//                 if(strpos($user, $username.':') !== false) {
+//                     $result = $user;
+//                 }
+//             }
+//         }
 
         foreach ($this->users as $user) {
             if(strpos($user['username'], $username) !== false) {
                 $result = $user;
             }
         }
-
         return $result;
+    }
+    
+    public function isAuth() {
+        $isAuth = false;
+        
+        session_start();
+        if(isset($_SESSION['admin'])) {
+            $isAuth = $_SESSION['admin'];
+        }
+        session_write_close();
+
+        return $isAuth;
     }
 }
