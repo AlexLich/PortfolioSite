e72c84075a21d730d195e294b4f74780009f82e7
Alex Lich <psixwill@gmail.com>
2016-06-11 14:57:58 +0300

    пытаюсь админку допилить хотя бы удаленее новостей (хз что должно быть в инклуд)

--- src/app/views/adminView.php
+++ src/app/views/adminView.php
@@ -1,8 +1,13 @@
 <title>Админка</title>
       <div class="content2">
+        <?php 
+          include $contentView; 
+        ?>
          <p>тут хрень добавить, хрень удалить</p>          
       </div>
    <div class="content4">
     <p><a href="/logout">Exit</a></p>
+    <p><a href="/logout">Дабавить новость</a></p>
+    <p><a href="/delnews">Удалить новости</a></p>
     <p> тут ссылки</p>
 </div>
