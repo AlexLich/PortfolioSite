a27b145e4217be4259c39ba4040b6bddaa8ad3cd
iGilga <pyua87@gmail.com>
2016-06-12 00:14:21 +0300

    Добавлен параметр авторизация

--- src/app/core/view.php
+++ src/app/core/view.php
@@ -11,7 +11,12 @@ class View
         $this->templateView = 'templateView.php';
     }
 
-    function generate($contentView, $data = null)
+    // function generate($contentView, $data = null)
+    // {
+    //     include $this->dirViews.$this->templateView;
+    // }
+
+    function generate($contentView, $data = null, $isAuth = false)
     {
         include $this->dirViews.$this->templateView;
     }
