37bacdda951a75c8cd62b664e7a34c64c62a69c0
iGilga <pyua87@gmail.com>
2016-06-15 22:43:18 +0300

    Добавлены методы количество строк и получить часть данные.

--- src/app/services/articleService.php
+++ src/app/services/articleService.php
@@ -54,6 +54,7 @@ class ArticleService
         $pdo = $this->connect->getDb();
         if(!is_null($pdo)) {
             $count = $pdo->exec($sql);
+            $pdo = null;
         }
         return $count;
     }
@@ -65,6 +66,7 @@ class ArticleService
         $pdo = $this->connect->getDb();
         if(!is_null($pdo)) {
             $count = $pdo->exec($sql);
+            $pdo = null;
         }
         return $count;
     }
@@ -78,7 +80,31 @@ class ArticleService
         $pdo = $this->connect->getDb();
         if(!is_null($pdo)) {
             $count = $pdo->exec($sql);
+            $pdo = null;
         }
         return $count;
+    }
+
+    public function getCount()
+    {
+        $pdo = $this->connect->getDb();
+        if(!is_null($pdo)) {
+            $count = $pdo->query('SELECT count(*) FROM articles')->fetchColumn();
+            $pdo = null;
+        }
+        return (int) $count;
+    }
+
+    public function getSegment($start, $limit)
+    {
+        $data = null;
+        $sql="SELECT id, name, content, UNIX_TIMESTAMP(datetime) as dt FROM articles LIMIT $start, $limit";
+        $pdo = $this->connect->getDb();
+        if(!is_null($pdo)) {
+            $data = $pdo->query($sql)->fetchAll(PDO::FETCH_CLASS, "App\\Model\\Article");
+            $pdo = null;
+        }
+
+        return $data;
     }
 }
